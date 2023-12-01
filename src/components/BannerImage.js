14e78ee6b4437d5215ae9b0514fee59860b61af2
jatink124 <kaushaljatin48@gmail.com>
2023-12-01 18:44:31 +0530

    saff

--- src/components/BannerImage.js
+++ src/components/BannerImage.js
@@ -1,16 +1,28 @@
 import React from 'react';
 
-const BannerImage = ({ imageUrl, altText, linkUrl }) => {
+const linkStyle = {
+  position: 'absolute',
+  top: '50%', // Adjust the positioning based on your specific needs
+  left: '50%', // Adjust the positioning based on your specific needs
+  transform: 'translate(-50%, -50%)',
+  width: '80%', // Adjust the size of the transparent link
+  height: '3rem', // Adjust the size of the transparent link
+  cursor: 'pointer',
+  backgroundColor: 'rgba(0, 0, 0, 0)', // Transparent background color
+  border: 'none',
+  outline: 'none',
+};
+
+const BannerImage = ({ imageUrl, altText }) => {
   return (
     <div className="relative">
       <img
         src={imageUrl}
         alt={altText}
-        className="w-full h-auto rounded-lg shadow-md sm:w-full sm:h-full"
+        className="w-full h-auto"
       />
-      <a href={linkUrl} className="absolute top-0 left-0 w-full h-full rounded-lg bg-transparent hover:bg-opacity-20 transition duration-300 ease-in-out">
-        {/* Add screen readers accessibility if needed */}
-        <span className="sr-only">Click here to visit the card page</span>
+      <a href="/card" style={linkStyle} className="hover:bg-opacity-20 transition duration-300 ease-in-out sm:w-1/2 sm:h-12">
+        {/* You can add content inside the link if needed */}
       </a>
       {/* Add any overlay or content over the image if needed */}
       {/* Example: <div className="absolute top-0 left-0 right-0 bottom-0 text-white">Your Content</div> */}
